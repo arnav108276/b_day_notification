e86431532daa5d4f3722929cca27742ad65d970a
Rangoli Panwar <143282697+rangolipanwar@users.noreply.github.com>
2023-10-27 20:04:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,47 @@
 import datetime
-
 import json
-
 import mysql.connector
-
 from plyer import notification
-
 import pyttsx3
 
 DB_CONFIG = {
-    
     'host': 'localhost',
-    
     'user': 'root',
-    
     'password': 'passkey=00',
-    
     'database': 'devops',
-    
-}
+    }
 
 BIRTHDAYS_FILE = 'birthdays.json'
-
 def load_birthdays():
-    
     try:
-        
-        with open(BIRTHDAYS_FILE, 'r') as file:
-            
+         with open(BIRTHDAYS_FILE, 'r') as file:
             return json.load(file)
-            
-    except FileNotFoundError:
-        
+        except FileNotFoundError:
         return {}
-        
-def schedule_notifications():
-    
-    today = datetime.date.today()
-    
-    birthdays = load_birthdays()
 
-    
+
+def schedule_notifications():
+    today = datetime.date.today()
+     birthdays = load_birthdays()
+
     for birthday, name in birthdays.items():
-        
-        month, day = map(int, birthday.split())
-        
-        birthday_date = datetime.date(today.year, month, day)
-        
+         month, day = map(int, birthday.split())
+         birthday_date = datetime.date(today.year, month, day)
         days_until_birthday = (birthday_date - today).days
 
-        
         if days_until_birthday == 0:
-            
-            message = f"Today is {name}'s birthday! 🎉"
-            
-            speak("Birthday notification")
-            
-            notification.notify(
+             message = f"Today is {name}'s birthday! 🎉"
+             speak("Birthday notification")
+             notification.notify(
                 
                 title='Birthday Notification',
                 message=message,
                 app_icon="C:\\Users\\Arnav\\OneDrive\\Documents\\cake.ico",
                 app_name='Birthday Reminder',
                 timeout=10,
-            )
-            
+            )     
+
+
 def add_birthday():
     month = int(input("Enter the month (1-12): "))
     day = int(input("Enter the day (1-31): "))
@@ -86,6 +63,7 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 if __name__ == "__main__":
+    
     while True:
         choice = input("Enter '1' to add a birthday or '2' to check for upcoming birthdays (q to quit): ")
         if choice == '1':
